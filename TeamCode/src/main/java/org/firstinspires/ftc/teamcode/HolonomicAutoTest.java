42cece5d204dbb61377a3ec9f04566fd6221c01b
cdavidson22 <cdavidson22@weldre4.org>
2020-11-11 11:40:09 -0700

    Fixed motor directions in autonomous test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HolonomicAutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HolonomicAutoTest.java
@@ -1,4 +1,73 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-public class HolonomicAutoTest {
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.ftccommon.ClassManagerFactory;
+import com.qualcomm.robotcore.hardware.CRServo;
+import java.math.*;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
+@Autonomous(name="Honolomic Autonomous Test", group="autonomous")
+//@Disabled
+public class HolonomicAutoTest extends LinearOpMode{
+    private DcMotor F1 = null;
+    private DcMotor F2 = null;
+    private DcMotor R1 = null;
+    private DcMotor R2 = null;
+
+    double powerF1;
+    double powerF2;
+    double powerR1;
+    double powerR2;
+
+    double Pi = Math.PI/4;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+
+        F1 = hardwareMap.get(DcMotor.class, "F1");
+        F2 = hardwareMap.get(DcMotor.class, "F2");
+        R1 = hardwareMap.get(DcMotor.class, "R1");
+        R2 = hardwareMap.get(DcMotor.class, "R2");
+
+        F1.setDirection(DcMotorSimple.Direction.REVERSE);
+        F2.setDirection(DcMotorSimple.Direction.FORWARD);
+        R1.setDirection(DcMotorSimple.Direction.FORWARD);
+        R2.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        F1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        F2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        R1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        R2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+        telemetry.addData("version","1.0");
+
+        sleep(1000);
+
+        F1.setPower(.25);
+        F2.setPower(.25);
+        R1.setPower(.25);
+        R2.setPower(.25);
+        sleep(1750);// moves forward off balence board
+
+        F1.setPower(0);
+        F2.setPower(0);//stops
+        R1.setPower(0);
+        R2.setPower(0);
+    }
 }
